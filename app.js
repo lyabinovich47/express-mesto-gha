4da7e717ed73817256d1b2c659a8238a5e7e5430
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-04-08 13:04:30 +0300

    еще раз поправил обработчик неправильного пути

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 
   next();
 });
-app.use('/', router);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const cardRoutes = require('./cards');
 
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
-router.use((req, res) => {
+router.use('*', (req, res) => {
   res.statusCode(404).send({ message: 'Страница по указанному пути не найдена' });
 });
 
