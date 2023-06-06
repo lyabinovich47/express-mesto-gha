e65e6796a8f24ef4b1b7ea0da48078e0b0c275d6
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-06 18:09:47 +0300

    поправил валадицию на uri() в createUser и updateAvatar

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-const regExpUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?#?$/;
+// const regExpUrl = /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?$/;
 
 // /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?#?$/;
 
-module.exports = regExpUrl;
+// module.exports = regExpUrl;
