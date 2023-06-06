34fbe914fd1864116000c4c86471b57db379e394
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-06 18:59:57 +0300

    добавил uri() для валидации ссылки на картинку в validateUserBody

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -5,7 +5,7 @@ const validateUserBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
+    avatar: Joi.string().uri(),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
