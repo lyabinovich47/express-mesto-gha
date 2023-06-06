08b492a7c7030379eadbef9f7efeaf76d4f63c1e
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-06 18:37:45 +0300

    подправил валидацию

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
-const regExpUrl = /^https?:\/\/(www.)?[a-zA-Z0-9-._~:/?#[]@!$&'()+,;=]#?$/;
+// const regExpUrl = /^https?:\/\/(www.)?[a-zA-Z0-9-._~:/?#[]@!$&'()+,;=]#?$/;
 
 // /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?#?$/;
 
-module.exports = regExpUrl;
+// module.exports = regExpUrl;
