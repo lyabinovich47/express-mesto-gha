0e8a8ffa36ab105e704216c0051ed7c70ddbbfb0
Lyabin Andrey <lyabinovich47@yandex.ru>
2023-06-07 12:11:14 +0300

    поправил валидацию ссылок через isUrl и custom(isUrl)

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -1,11 +1,12 @@
 const { celebrate, Joi } = require('celebrate');
 // const regExpUrl = require('../utils/constants');
+const isUrl = require('../utils/constants');
 
 const validateUserBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri(),
+    avatar: Joi.string().custom(isUrl),
     email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
@@ -33,14 +34,14 @@ const validateUserProfileBody = celebrate({
 
 const validateUserAvatarBody = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string(),
+    avatar: Joi.string().custom(isUrl),
   }),
 });
 
 const validateCardBody = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().custom(isUrl),
   }),
 });
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,23 @@
-// const regExpUrl = /^https?:\/\/(www.)?[a-zA-Z0-9-._~:/?#[]@!$&'()+,;=]#?$/;
+const validator = require('validator');
+
+const isUrl = (link) => {
+  const result = validator.isURL(link);
+  if (result) {
+    return link;
+  }
+  throw new Error('Невалидная ссылка');
+};
+
+module.exports = isUrl;
+// от Макса
+// const regExpUrl = /^https?:\/\/(www.)?[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]*#?$/;
 
 // const regExpUrl2 = /^(https?:\/\/)?([\w\.]+)\.([a-z]{2,6}\.?)(\/[\w\.]*)*\/?#?$/;
+
+// const reUrl3 = /[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/;
 
 // /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?#?$/;
 
 // module.exports = regExpUrl;
+// от Юры
+// const regExpUrl2 = /(https?:\/\/)(w{3}.)?(([a-zA-Z0-9]+).)+/;
